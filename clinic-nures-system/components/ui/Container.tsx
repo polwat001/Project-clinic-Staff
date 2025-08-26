2bcb1508a6a7fe2881fdb0db81ce1ed37206f0a5
polwat <polwatboonlang@gmail.com>
2025-08-26 18:33:22 +0700

    Revert "pooo"
    This reverts commit 882f61abdbc01e9c817bf4176753594001913063.
    
    Revert "Update tsconfig.json"
    
    This reverts commit 62beb40e086d421b8c61e763349bd104c169a7fa.

--- clinic-nures-system/components/ui/Container.tsx
+++
@@ -1,7 +0,0 @@
-export default function Container({ children, className = "" }) {
-  return (
-    <div className={`bg-white border-2 border-[#D9D9D9] rounded-[25px] p-8 shadow ${className}`}>
-      {children}
-    </div>
-  );
-}
